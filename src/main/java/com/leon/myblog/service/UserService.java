2bddb815299fa54c7ae0d16b1a2993bb85c928a7
miaopasi <1429169422@qq.com>
2019-11-13 21:56:37 +0800

    新增根据用户ID查询该用户角色接口

--- src/main/java/com/leon/myblog/service/UserService.java
+++ src/main/java/com/leon/myblog/service/UserService.java
@@ -1,9 +1,13 @@
 package com.leon.myblog.service;
 
+import com.leon.myblog.enity.RoleHasUserKey;
 import com.leon.myblog.enity.User;
+import com.leon.myblog.mapper.RoleHasUserMapper;
 import com.leon.myblog.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 
 /**
@@ -19,6 +23,8 @@ public class UserService {
     @Autowired
     private UserMapper  userMapper;
 
+    @Autowired
+    private RoleHasUserMapper roleHasUserMapper;
 
     public User findByUserName(String username) {
         return userMapper.get(username);
@@ -26,5 +32,9 @@ public class UserService {
 
     public int insertUser(User user){
         return userMapper.insert(user);
+    }
+
+    public List<RoleHasUserKey> getRoleByUserId(int id) {
+        return roleHasUserMapper.getRoleById(id);
     }
 }
