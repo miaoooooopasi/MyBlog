8d949fb966c30a46a38e6daabc175eb797d615fc
jiyang <1429169422@qq.com>
2019-11-18 16:12:51 +0800

    集成了thymeleaf模板base.html

---
+++ src/main/java/com/leon/myblog/controller/AdminController.java
@@ -0,0 +1,27 @@
+package com.leon.myblog.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * @author ：leon
+ * @date ：Created in 2019-11-15 9:04
+ * @description：${description}
+ * @modified By：
+ * @version: $version$
+ */
+
+@RestController
+@RequestMapping("/admin")
+public class AdminController {
+
+    @GetMapping("/home")
+    public ModelAndView home(){
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("/admin/base.html");
+        return mv;
+    }
+
+}
