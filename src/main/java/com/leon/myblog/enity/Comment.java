b33e2b166b489ad0dfe23b178410342293f97321
jiyang <1429169422@qq.com>
2020-07-08 16:55:29 +0800

    新增评论表的mapper

---
+++ src/main/java/com/leon/myblog/enity/Comment.java
@@ -0,0 +1,85 @@
+package com.leon.myblog.enity;
+
+import java.util.Date;
+
+public class Comment {
+    private Integer id;
+
+    private String nickname;
+
+    private String email;
+
+    private String content;
+
+    private String avatar;
+
+    private Date createtime;
+
+    private String parentcommentid;
+
+    private Integer articleId;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public Date getCreatetime() {
+        return createtime;
+    }
+
+    public void setCreatetime(Date createtime) {
+        this.createtime = createtime;
+    }
+
+    public String getParentcommentid() {
+        return parentcommentid;
+    }
+
+    public void setParentcommentid(String parentcommentid) {
+        this.parentcommentid = parentcommentid;
+    }
+
+    public Integer getArticleId() {
+        return articleId;
+    }
+
+    public void setArticleId(Integer articleId) {
+        this.articleId = articleId;
+    }
+}
